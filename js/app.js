79055d6466b67e4f6a3ddc69cf6fd5c5a2531a8b
Belinda Caylor <belinda.caylor@gmail.com>
2019-12-30 15:08:26 -0600

    Code cleanup

--- js/app.js
+++ js/app.js
@@ -18,7 +18,6 @@
  *
 */
 const sections = document.querySelectorAll('section');
-// const navBar = document.getElementByID('navbar__list');
 const navBar = document.querySelector('ul#navbar__list');
 let activeClass = "section1";
 /**
@@ -38,16 +37,9 @@ let activeClass = "section1";
 function htmlTextToAdd() {
   let navList = ""
   sections.forEach(function(sections) {
-    // navList = sections.dataset.nav
   navList = navList + "<a href=\"\#" + sections.id + "\">"
   navList = navList + "<li data-id=\"" + sections.id + "\">" + sections.dataset.nav + "</li></a>";});
-  // navList = navList + "<a href=\"\#" + sections.id + "\"><li>" + sections.dataset.nav + "</li></a>";});
-  // navList = navList + "<a href=\"\#section3\"><li>" + sections.dataset.nav + "</li></a>";});
-  // navList = navList + "<li><a href=\"\#section3\">Section 1</a></li>";});
-  // navList = navList + "<li>" + sections.dataset.nav + "</li>";});
-  // console.log navList;
   return navList;
-  // return '<li>Section 1</li>';
 }
 
 
@@ -71,17 +63,6 @@ function changeActive(newSection) {
 */
 
 // Build menu with anchor link
-// navBar.insertAdjacentHTML('afterbegin', htmlTextToAdd());
 navBar.insertAdjacentHTML('afterbegin', htmlTextToAdd());
-
-// Set sections as active
-// navBar.addEventListener('click', changeActiveClass('test'));
-// querySelector('#navbar__list').addEventListener('click', changeActiveClass());
-// navBar.addEventListener('click', function changeActive() {
-//   var removeClass = document.getElementById(activeClass);
-//   removeClass.classList.remove("your-active-class");
-//   var addClass = document.getElementById(event.target.dataset.id);
-//   addClass.classList.add("your-active-class");
-//   activeClass = event.target.dataset.id;
-// });
+//Listen for click on navigation bar and change active class.
 navBar.addEventListener('click', function () {changeActive(event.target.dataset.id)});
